a6ea040697c39f3a67f83d59907acc3da1f7762a
Damilare Tolulope <damilaretolulope1@gmail.com>
2023-09-07 15:47:06 +0100

    chore: remove pagination, status

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,40 +1,29 @@
 
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { cardData } from '../../common/CardData'
 import Card from '../../components/Card'
-import Paginate from '../../components/Pagination'
 import Table from '../../components/Table'
 import { getRequest } from '../../utils/Api'
 import withScrollToTop from '../../utils/WithScrollToTop.'
 
 const Users = () => {
-  const [userData, setUserData] = useState([])
-  const [dataCards] = useState(cardData)
-
-  // Table Paginate
-  const [pageCount, setPageCount] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [records, setRecords] = useState(0);
+  const [userData, setUserData] = useState([]);
+  const [dataCards] = useState(cardData);
 
   const fetchData = () => {
     try{
       const response = getRequest(process.env.REACT_APP_USERS_URL)
       response.then( res => {
         setUserData(res.data)
-        setPageCount(userData.length/10)
-        console.log(userData.length/10)
       });
     } catch(error){
       console.log(error)
     }
   }
 
-  const handlePageClick = event => {
-      setCurrentPage(event.selected + 1);
-};
 
   const headings = [
-    'Organization', 'username', 'email', 'phone number', 'date joined', 'status'
+    'Organization', 'username', 'email', 'phone number', 'date joined',
   ]
 
 
@@ -56,8 +45,6 @@ const Users = () => {
       <div>
         <Table headings={headings} bodyData={userData} />
       </div>
-
-        <Paginate pageCount={pageCount} setPageCount={setPageCount} records={userData} />
 
     </div>
   )
