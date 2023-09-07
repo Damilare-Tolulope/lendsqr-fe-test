bb1452b64f1a2f1732109a0d926a5e190d22b2a0
Damilare Tolulope <damilaretolulope1@gmail.com>
2023-09-07 15:46:13 +0100

    choreL pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,7 +1,40 @@
-const Paginate = ({pageCount, setPageCount, records}) => {
-    return (
-        <div></div>
-    )
-}
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const maxPage = 5
+    const handleNextPage = () => {
+      if (currentPage < totalPages) {
+        onPageChange(currentPage + 1);
+      }
+    };
+  
+    const handlePrevPage = () => {
+      if (currentPage > 1) {
+        onPageChange(currentPage - 1);
+      }
+    };
 
-export default Paginate
+    console.log([...Array(totalPages)], currentPage, totalPages)
+  
+    return (
+      <div className="pagination">
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Prev
+        </button>
+
+        {
+            totalPages > maxPage ?
+            <>
+                {[...Array(totalPages)].slice(0, maxPage).map((page, index) => <p key={index} className={`${(index + 1) === currentPage ? "selected" : ""}`}>{index + 1}</p>)}
+                ...
+            </>
+            :
+            [...Array(totalPages)].map((page, index) => <p key={index} className={`${(index + 1) === currentPage ? "selected" : ""}`}>{index + 1}</p>)
+        }
+        
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+    );
+  };
+
+export default Pagination;
--- src/components/Table.js
+++ src/components/Table.js
@@ -4,6 +4,7 @@ import filter from '../assets/images/icons/filter.svg'
 import moment from 'moment'
 import { usePopper } from 'react-popper'
 import { Link, useNavigate } from 'react-router-dom'
+import Pagination from './Pagination'
 
 const Table = ({headings, bodyData}) => {
         const [popperElement, setPopperElement] = useState(null);
@@ -25,34 +26,53 @@ const Table = ({headings, bodyData}) => {
             document.body.style.overflow = "scroll";
         };
 
+        // Pagination
+
+        const pageSize = 6;
+        const [currentPage, setCurrentPage] = useState(1);
+      
+        const startIndex = (currentPage - 1) * pageSize;
+        const endIndex = startIndex + pageSize;
+      
+        const currentData = bodyData.slice(startIndex, endIndex);
+      
+        const totalPages = Math.ceil(bodyData.length / pageSize);
+      
+        const handlePageChange = (newPage) => {
+          setCurrentPage(newPage);
+        };
+        
+
   return (
     <>
-    { !bodyData ? <p>Unable to load data</p> :  
-        <table>
-            <thead>
-                <tr>
-                    {headings.map((head, index) => <td key={index}>{head} <img src={filter} alt="filter" /></td>)}
-                    <td></td>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    bodyData.map(({orgName, userName, email, phoneNumber, createdAt, id }, index) => {
-                        return (
-                            <tr onClick={() => navigate(`/${id}`)} key={id}>
-                                <td>{orgName}</td>
-                                <td>{userName}</td>
-                                <td>{email}</td>
-                                <td>{phoneNumber}</td>
-                                <td>{moment(createdAt).format('lll')}</td>
-                                <td>Pending</td>
-                                <td><img ref={setReferrenceElement} src={more} alt="more" /></td>
-                            </tr>
-                        )
-                    })}
-            </tbody>
-        </table>
-    }
+    <div>
+        { !bodyData ? <p>Unable to load data</p> :  
+            <table>
+                <thead>
+                    <tr>
+                        {headings.map((head, index) => <td key={index}>{head} <img src={filter} alt="filter" /></td>)}
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        currentData.map(({orgName, userName, email, phoneNumber, createdAt, id }) => {
+                            return (
+                                <tr onClick={() => navigate(`/${id}`)} key={id}>
+                                    <td>{orgName}</td>
+                                    <td>{userName}</td>
+                                    <td>{email}</td>
+                                    <td>{phoneNumber.split(' x')[0]}</td>
+                                    <td>{moment(createdAt).format('lll')}</td>
+                                    <td><img ref={setReferrenceElement} src={more} alt="more" /></td>
+                                </tr>
+                            )
+                        })}
+                </tbody>
+            </table>
+        }
+        <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+    </div>
     <div ref={setPopperElement} {...attributes.popper}
         style={{ ...styles.popper, position: "absolute", zIndex: "30", background: "#fff", marginTop: "1rem", padding: "1rem" }}
     >
